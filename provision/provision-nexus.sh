803e22851830b6a713e2390eb3cb6a1890c18c00
Rui Lopes <rgl@ruilopes.com>
2017-11-16 22:57:30 +0000

    upgrade to nexus 3.6.1-02

--- provision/provision-nexus.sh
+++ provision/provision-nexus.sh
@@ -31,9 +31,9 @@ install -d -o root -g nexus -m 750 /opt/nexus
 pushd /opt/nexus
 # see http://www.sonatype.com/download-oss-sonatype
 # see https://help.sonatype.com/display/NXRM3
-nexus_tarball=nexus-3.6.0-02-unix.tar.gz
+nexus_tarball=nexus-3.6.1-02-unix.tar.gz
 nexus_download_url=https://sonatype-download.global.ssl.fastly.net/nexus/3/$nexus_tarball
-nexus_download_sha1=8278ecb4969e2a2eb3abe373cb1914a4b863160c
+nexus_download_sha1=da2ea7cfe282b9372f818c011c28c8c41246ae24
 wget -q $nexus_download_url
 if [ "$(sha1sum $nexus_tarball | awk '{print $1}')" != "$nexus_download_sha1" ]; then
     echo "downloaded $nexus_download_url failed the checksum verification"
