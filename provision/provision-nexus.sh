663445b54fa9b910583f8aef0a30b44e6fe5c6f6
Rui Lopes <rgl@ruilopes.com>
2020-05-09 08:08:03 +0100

    upgrade to nexus-3.23.0-03

--- provision/provision-nexus.sh
+++ provision/provision-nexus.sh
@@ -33,17 +33,17 @@ pushd /opt/nexus
 # see https://www.sonatype.com/download-oss-sonatype
 # see https://help.sonatype.com/repomanager3/download/download-archives---repository-manager-3
 # see https://help.sonatype.com/display/NXRM3
-nexus_version=3.22.0-02
+nexus_version=3.23.0-03
 nexus_home=/opt/nexus/nexus-$nexus_version
 nexus_tarball=nexus-$nexus_version-unix.tar.gz
-nexus_download_url=https://sonatype-download.global.ssl.fastly.net/nexus/3/$nexus_tarball
-nexus_download_sha1=cccf70509def19c1b0db644a62cc57922f9f86b7
+nexus_download_url=https://download.sonatype.com/nexus/3/$nexus_tarball
+nexus_download_sha1=8298bd6db3b0f4d083942343a89f47cae7d791de
 wget -q $nexus_download_url
 if [ "$(sha1sum $nexus_tarball | awk '{print $1}')" != "$nexus_download_sha1" ]; then
     echo "downloaded $nexus_download_url failed the checksum verification"
     exit 1
 fi
-tar xf $nexus_tarball # NB this creates the $nexus_home (e.g. nexus-3.22.0-02) and sonatype-work directories.
+tar xf $nexus_tarball # NB this creates the $nexus_home (e.g. nexus-3.23.0-03) and sonatype-work directories.
 rm $nexus_tarball
 install -d -o nexus -g nexus -m 700 .java # java preferences are saved here (the default java.util.prefs.userRoot preference).
 install -d -o nexus -g nexus -m 700 sonatype-work/nexus3/etc
