79146970aba0f90ca9f43be08091362123c7d0b4
Rui Lopes <rgl@ruilopes.com>
2022-12-03 10:18:14 +0000

    upgrade to go 1.19

--- provision/use-docker-repository.sh
+++ provision/use-docker-repository.sh
@@ -61,10 +61,10 @@ EOF
 cat >go.mod <<'EOF'
 module example.com/go-hello
 
-go 1.18
+go 1.19
 EOF
 cat >Dockerfile <<'EOF'
-FROM golang:1.18-bullseye as builder
+FROM golang:1.19-bullseye as builder
 WORKDIR /app
 COPY go.* main.go ./
 RUN CGO_ENABLED=0 go build -ldflags="-s"
