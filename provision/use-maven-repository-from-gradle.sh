d000921bb3c200a65ba71e40824c1e400d5a5aea
Rui Lopes <rgl@ruilopes.com>
2023-07-29 06:59:59 +0100

    upgrade to gradle 8.2.1

--- provision/use-maven-repository-from-gradle.sh
+++ provision/use-maven-repository-from-gradle.sh
@@ -10,7 +10,7 @@ mkdir -p tmp/use-maven-repository-from-gradle && cd tmp/use-maven-repository-fro
 
 # download and install gradle.
 # see https://gradle.org/releases/
-gradle_version='8.0.2'
+gradle_version='8.2.1'
 if [ ! -f /opt/gradle/gradle-$gradle_version/bin/gradle ]; then
     apt-get install -y unzip
     wget -qO/tmp/gradle-$gradle_version-bin.zip https://services.gradle.org/distributions/gradle-$gradle_version-bin.zip
@@ -33,8 +33,8 @@ cat >settings.gradle <<'EOF'
 rootProject.name = 'gradle-greeter'
 EOF
 cat >build.gradle <<'EOF'
-// see https://docs.gradle.org/8.0.2/userguide/java_library_plugin.html
-// see https://docs.gradle.org/8.0.2/userguide/maven_plugin.html
+// see https://docs.gradle.org/8.2.1/userguide/java_library_plugin.html
+// see https://docs.gradle.org/8.2.1/userguide/maven_plugin.html
 
 plugins {
     id 'java-library'
@@ -98,8 +98,8 @@ cat >settings.gradle <<'EOF'
 rootProject.name = 'gradle-greeter-application'
 EOF
 cat >build.gradle <<EOF
-// see https://docs.gradle.org/8.0.2/userguide/java_plugin.html
-// see https://docs.gradle.org/8.0.2/userguide/application_plugin.html
+// see https://docs.gradle.org/8.2.1/userguide/java_plugin.html
+// see https://docs.gradle.org/8.2.1/userguide/application_plugin.html
 // see http://imperceptiblethoughts.com/shadow/
 
 plugins {
